78994c8896fb3b39fef62b7b9cfc1035411ffe4e
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-15 17:20:06 +0530

    change

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
     origin: "https://hi-coding-junction.netlify.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 
 // app.use(cors({
