e007e51d550bdda8c3a6fd29d07b6d12e36e2181
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-15 17:44:00 +0530

    change

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 
 // app.use(cors({
 //     origin: "http://localhost:5173",
