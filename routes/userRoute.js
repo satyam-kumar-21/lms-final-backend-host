eb772a8c6c24fe9481a4dcff6af3b7429256cb84
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-15 15:59:31 +0530

    change

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,12 +3,10 @@ import isAuth from "../middlewares/isAuth.js"
 import { getCurrentUser, UpdateProfile } from "../controllers/userController.js"
 import upload from "../middlewares/multer.js"
 
-
-
 let userRouter = express.Router()
 
-userRouter.get("/currentuser",isAuth,getCurrentUser)
-userRouter.post("/updateprofile",isAuth,upload.single("photoUrl"),UpdateProfile)
+userRouter.get("/currentuser", isAuth, getCurrentUser)
+userRouter.post("/updateprofile", isAuth, upload.single("photoUrl"), UpdateProfile)
 
 
 export default userRouter
