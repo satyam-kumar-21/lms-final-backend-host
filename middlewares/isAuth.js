2065ecda03d9a277afaad06511e55c95bc0ca63b
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-17 17:19:44 +0530

    change

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -1,24 +1,25 @@
-
 import jwt from "jsonwebtoken"
-const isAuth=async (req,res,next)=>{
+const isAuth = async(req, res, next) => {
     try {
- 
-      let {token} = req.cookies
-     
-      if(!token){
-        return res.status(400).json({message:"user doesn't have token"})
-      }
-      let verifyToken = jwt.verify(token,process.env.JWT_SECRET)
-      
-      if(!verifyToken){
-        return res.status(400).json({message:"user doesn't have valid token"})
-      }
-  
-      req.userId = verifyToken.userId
-      next()
+
+        console.log("Cookies:", req.cookies); // 🔥
+        console.log("Origin:", req.headers.origin);
+        let { token } = req.cookies
+
+        if (!token) {
+            return res.status(400).json({ message: "user doesn't have token" })
+        }
+        let verifyToken = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (!verifyToken) {
+            return res.status(400).json({ message: "user doesn't have valid token" })
+        }
+
+        req.userId = verifyToken.userId
+        next()
     } catch (error) {
         console.log(error)
-        return res.status(500).json({message:`is auth error ${error}`})
+        return res.status(500).json({ message: `is auth error ${error}` })
     }
 }
 export default isAuth
