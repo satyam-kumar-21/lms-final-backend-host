c982a3b1611d70e1459802a781b6103f60a9d6d0
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-18 01:17:16 +0530

    final

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,8 +1,9 @@
 import multer from "multer";
+import os from "os";
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./public")
+        cb(null, os.tmpdir())
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname)
