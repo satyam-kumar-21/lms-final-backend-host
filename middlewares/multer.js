8fe2154d85ca91932582c89bf1f7e30ae33b38cc
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-17 17:27:41 +0530

    change

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,14 +1,29 @@
+// import multer from "multer";
+
+// let storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, "./public")
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.originalname)
+//     }
+// })
+
+// const upload = multer({ storage })
+
+// export default upload
+
+
+
 import multer from "multer";
 
-let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./public")
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 100 * 1024 * 1024 // 100MB (videos)
     }
-})
+});
 
-const upload = multer({ storage })
-
-export default upload
+export default upload;
