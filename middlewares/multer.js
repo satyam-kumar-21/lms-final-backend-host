2c7922b2e66b99b9f66d130877015f6a2e6f9062
Satyam kumar <satyamkumar2716@gmail.com>
2025-12-17 16:12:27 +0530

    chnge

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,28 +1,14 @@
-// import multer from "multer";
-
-// let storage = multer.diskStorage({
-//     destination:(req,file,cb)=>{
-//         cb(null,"./public")
-//     },
-//     filename:(req,file,cb)=>{
-//         cb(null,file.originalname)
-//     }
-// })
-
-// const upload = multer({storage})
-
-// export default upload
-
-
-
-
-
-
-
 import multer from "multer";
 
-// Memory storage works on local and serverless (Vercel)
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./public")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
 
-export default upload;
+const upload = multer({ storage })
+
+export default upload
